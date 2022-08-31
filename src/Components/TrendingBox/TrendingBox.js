67a78e1c945157b7d0bc7a1535f9f4c4fd71e03f
Naveen Sharma <103960076+NavGithub15@users.noreply.github.com>
2022-08-31 17:27:49 -0400

    Add gitignore and add trending card in compoenent

---
+++ src/Components/TrendingBox/TrendingBox.js
@@ -0,0 +1,11 @@
+import TrendingCard from "../TrendingCard/TrendingCard";
+
+export default function TrendingBox() {
+  return (
+    <>
+    <div className="trending__container">
+    <TrendingCard />
+    </div>
+    </>
+  )
+}
---
+++ src/Components/TrendingCard/TrendingCard.js
@@ -0,0 +1,5 @@
+export default function TrendingCard() {
+  return (
+    <div>TrendingCard</div>
+  )
+}
