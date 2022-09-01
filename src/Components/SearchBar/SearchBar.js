872366d015dcb61c1e2802c414a0fb1d03f9eeb2
Robert Loo <rloolee89@gmail.com>
2022-09-01 15:19:07 -0400

    Finalize styling for hackathon

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,23 +1,40 @@
 import "./SearchBar.scss";
 import React from 'react';
+import {useState} from "react";
+import Modal from "../Modal/Modal";
+
 
 const SearchBox = ({ value, setValue, onSearch}) => {
-	const onClick = (e) => {
-		e.preventDefault();
-		onSearch();
+	const [modal, setModal] = useState(false);
+	const  [search, setSearch] = useState("");
+
+
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+	
+		if (!event.target.search.value) {
+			console.log("empty")
+			 return setModal(true)
+		} else 	(onSearch());
+		
 	}
 
 	return (
 		<div className="searchbar">
-		<form className="searchbar__form">
+		<form   onSubmit={handleSubmit}className="searchbar__form">
 			<input
+			
 				className='searchbar__input'
+				name="search"
 				value={value}
 				onChange={(event) => setValue(event.target.value)}
 				placeholder='Type to search...'
 			></input>
-			<button className="searchbar__btn" onClick={onClick}>Search</button>
+			<button className="searchbar__btn" type="submit" >Search</button>
 		</form>
+		<Modal open ={modal} onClose={() => setModal(false)}/>
+		{/* {<modal && Modal />} */}
 		</div>
 	);
 };
--- src/pages/TvShows/TvShows.js
+++ src/pages/TvShows/TvShows.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import ShowsNav from "../../Components/ShowsNav/ShowsNav";
 import "./TvShows.scss";
-
-
+import logo from "../HomePage/logo-2.jpg"
 const API_URL="https://api.themoviedb.org/3/tv/"
 const REST_URL="?api_key=de66be7a82e7cc167b3ecb1b850cbc1c&language=en-US";
 const IMAGE_API = "https://image.tmdb.org/t/p/w500"
@@ -26,26 +24,22 @@ const TvShows = () => {
   }, [])
 
   return (
-    <>
-    <ShowsNav />
-
-
-
-        <div className="show-image" style={{backgroundImage:`url(${IMAGE_API}${showDetail.backdrop_path})`}}>
-        <Link className="btn__link" to="/"><button className="btn">BACK</button></Link>
+    <section className="show-container">
+        <div className="show-image" style={{backgroundImage:`linear-gradient(rgba(50, 50, 50, 0.5), rgba(50, 50, 50, 0.5)),url(${IMAGE_API}${showDetail.backdrop_path})`}}>
           <div className="show-image__container">
             <div className="show-image__wrapper">
           {showDetail.poster_path && <img  className="show-image__img"src={`${IMAGE_API}${showDetail.poster_path}`}/>}
-          </div>
           </div>
           <div className="show-image__detail">
           <h1>{showDetail.name}</h1>
           <h2>{showDetail.homepage}</h2>
           <p>{showDetail.overview}</p>
           </div>
-
+          </div>
         </div>
-    </>
+          <div className="overlay"></div>
+          <Link className="btn__link" to="/"><button className="btn">BACK</button></Link>
+    </section>
   );
 };
 
